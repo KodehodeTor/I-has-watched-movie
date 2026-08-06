44845c87b2559dfacfb114d5fd9bd0e0abb47de5
KodehodeTor <torjarand.26pg@gmail.com>
2026-08-06 13:27:17 +0200

    Actually completed cardCrate function...

--- CreateCard.js
+++ CreateCard.js
@@ -30,4 +30,25 @@ export function cardCreate(txt) {
     }
     watchedSaver(txt.recommended, txt.movie, "recommended");
   });
+
+  const check = document.createElement("input");
+  check.type = "checkbox";
+
+  check.checked = txt.watchedSaver;
+
+  const label = document.createElement("label");
+  const labelTxt = document.createTextNode("Wached?: ");
+  label.append(labelTxt);
+  label.append(check);
+
+  label.addEventListener("click", (e) => {
+    txt.watched = !txt.watched;
+    watchedSaver(txt.watched, txt.movie, "done");
+  });
+
+  p.append(text);
+  div.appendChild(p);
+  div.appendChild(img);
+  div.appendChild(label);
+  cardCont.appendChild(div);
 }
