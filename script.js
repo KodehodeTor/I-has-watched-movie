0b03617bc5a3ac2abb1daca524a58b4ac30e22db
KodehodeTor <torjarand.26pg@gmail.com>
2026-08-06 12:30:11 +0200

    Added notWatched icon as standard, startCheck added aswell.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //Film- eller serieliste: Et system for å logge hvilke filmer og serier som er sett, samt gi vurderinger.
 
 //Variables 
+export const notWatched =document.querySelector(#notWatched)
 const cardCone = document.querySelector(#cardCont);
 const movieInput = document.querySelector(#movie);
 const btnMovie = document.querySelector(#movieButton);
 const ratedBox = document.querySelector(#isWatched)
 const filter = document.querySelector(#filter)
+
+//Start check
+document.addEventListener("DOMContentLoaded", () => {
+    checkStorage();
+})
+
+//Watched function
