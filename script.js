3d77e385a540c7aec011572b511054e7a2c46ee6
KodehodeTor <torjarand.26pg@gmail.com>
2026-08-06 12:26:50 +0200

    Added const variables from HTML in script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Film- eller serieliste: Et system for å logge hvilke filmer og serier som er sett, samt gi vurderinger.
+
+//Variables 
+const cardCone = document.querySelector(#cardCont);
+const movieInput = document.querySelector(#movie);
+const btnMovie = document.querySelector(#movieButton);
+const ratedBox = document.querySelector(#isWatched)
+const filter = document.querySelector(#filter)
